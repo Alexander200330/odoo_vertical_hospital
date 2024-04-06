8f47ccb81982cc309281d0d2772b118798613c13
Alexander200330 <alexanderventura0630@gmail.com>
2024-04-05 23:41:22 -0400

    Validando que el código del tratamiento no tenga la secuencia 026

--- models/treatment.py
+++ models/treatment.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class Treatment(models.Model):
     _name = 'odoo_vertical_hospital.treatment'
@@ -7,3 +8,9 @@ class Treatment(models.Model):
     name = fields.Char(string='Código de tratamiento', required=True)
     treatment_name = fields.Char(string='Nombre del tratamiento')
     treating_physician = fields.Char(string='Médico tratante')
+    
+    @api.constrains('name')
+    def _check_name(self):
+        for record in self:
+            if '026' in record.name:
+                raise ValidationError("El código de tratamiento no puede contener la secuencia '026'.")
