487a9686d8a8e4d50c56312cb5902c1e19be1cd0
Alexander200330 <alexanderventura0630@gmail.com>
2024-04-05 23:13:32 -0400

    Configurando el chatter

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_vertical_hospital",
+    'name': "Vertical Hospital",
 
     'summary': """
         Aplicación para gestión hospitalaria que incluye registro de pacientes, generación de reportes en PDF,
@@ -34,7 +34,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
 
     # always loaded
     'data': [
--- models/patient.py
+++ models/patient.py
@@ -3,13 +3,14 @@ from odoo import models, fields, api, _
 class Patient(models.Model):
     _name = 'odoo_vertical_hospital.patient'
     _description = 'Formulario de Pacientes'
-
+    _inherit = 'mail.thread'
+    
     name = fields.Char(string='Número de Paciente', readonly=True)
     first_name = fields.Char(string='Nombre')
     last_name = fields.Char(string='Apellido')
-    rnc = fields.Char(string='RNC', size=11)
+    rnc = fields.Char(string='RNC', size=11, tracking=True)
     date_admission = fields.Datetime(string='Fecha de Alta', default=fields.Datetime.now)
-    state = fields.Selection([('draft', 'Borrador'), ('admission', 'Alta'), ('discharge', 'Baja')], string='Estado', default='draft')
+    state = fields.Selection([('draft', 'Borrador'), ('admission', 'Alta'), ('discharge', 'Baja')], string='Estado', default='draft', tracking=True)
 
     @api.model
     def create(self, vals):
